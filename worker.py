73fd503a90f0e90453869ddb999bd6711f997ed3
Mauricio Canales <mars20@utexas.edu>
2019-05-06 19:18:10 +0000

    worker.py edited online with Bitbucket

--- worker.py
+++ worker.py
@@ -1,7 +1,18 @@
 import jobs.py
+import datetime
+import json
 
-@......
+
+@queue.worker
 def execute_job(jid):
-    ......
+    #update status of job pending
+    update_job_status(jid,'Pending')
+    
+    #execute job
+    job = queue.get() # fix later
+    time.sleep(5)
+    
+    #update statue of job complete
+    update_json_statue(jid,'Complete')
 
 
