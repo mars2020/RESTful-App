e9c75d98186f1158ba3e656d13f048f60484d551
Sungsoo Kim <rickkim95@gmail.com>
2019-05-17 13:00:23 -0500

    ihi

--- worker.py
+++ worker.py
@@ -4,7 +4,7 @@ import json
 from app import *
 from hotqueue import HotQueue
 
-data = json.load(open('B-Cycle.json', 'r'))   
+d = pd.read_json(b_cycle())  
 
 @queue.worker
 def execute_job(jid):
@@ -15,14 +15,25 @@ def execute_job(jid):
     job = queue.get() # fix later
     time.sleep(5)
 
-def graph(name):
-    d = app.b_cycle()
+def graph(name):       
     name = name.replace('-',' ')
     graph = pd.value_counts(d[name]).plot.bar() # plotting graph
-    plt.show(graph)                                       # graph displayvh
+    plt.show(graph)                                       # graph display
     return '',200
 
     #update statue of job complete
     update_json_statue(jid,'Complete')
 
+def summary(name)
+   
+    name = name.replace('-',' ')
+    if name = "Checkout Date"
+    m = {
+        "field" : name,
+        "mean" : pd.d[name].mean(),
+        "Median" : pd.d[name].median(),
+        "Mode" : pd.d[name].mode(), 
+        "SD" : pd.d[name].std(), 
+        "Range" : ,
+    }
 
